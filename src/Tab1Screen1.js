1d1165df019adc8503af156964dfed1d87c83437
manuelzach <zachmanu@informatik.hu-berlin.de>
2018-12-27 20:02:03 +0100

    added testButton

--- src/Tab1Screen1.js
+++ src/Tab1Screen1.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import {View, Text, Button} from "react-native";
 
-const Tab1Screen1 = () => (
+const Tab1Screen1 = (props) => (
     <View style={{borderColor: "black", borderWidth: 1, flex: 1}}>
 
         <View style={{flex: 1, borderColor: "black", borderWidth: 1}}>
@@ -20,6 +20,9 @@ const Tab1Screen1 = () => (
 
         </View>
         <Text>Tab1Screen1fsdfasdfasd</Text>
+        <Button title={"zu 1/2"} onPress={() => {
+            props.navigation.navigate("Tab1Screen2")
+        }}/>
 
 
     </View>
